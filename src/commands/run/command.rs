dd82daf8e1d7afd2a0979f0e76e3a8989c732a3e
Manuel Penschuck <penschuck@imada.sdu.dk>
2025-12-19 17:34:36 +0100

    Implement communication with stride server and clean up the run command

--- src/commands/run/command.rs
+++ src/commands/run/command.rs
@@ -7,22 +7,102 @@ use crate::{
             summary_writer::SummaryWriter,
         },
     },
-    job::job_processor::{JobProcessor, JobProcessorBuilder, JobResult},
+    job::job_processor::{JobProcessorBuilder, JobResult},
     run_directory::*,
 };
+use std::collections::hash_set::IntoIter;
+use std::path::PathBuf;
 use std::{fs::File, sync::Arc};
 use thiserror::Error;
-use tracing::{debug, error, info};
+use tracing::{error, info};
 
 use crate::commands::run::upload::{JobResultUploadAggregation, UploadToStride};
-use crate::job::check_and_extract::SolutionInfos;
 use pace26remote::job_description::JobDescription;
 use pace26remote::upload::UploadError;
-use tokio::{
-    task::block_in_place,
-    time::{Duration, sleep},
-};
+use tokio::sync::{OwnedSemaphorePermit, Semaphore};
 use tokio::time::timeout;
+use tokio::time::{Duration, sleep};
+use pace26checker::digest::digest_output::InstanceDigest;
+use crate::job::check_and_extract::SolutionInfos;
+
+const DISPLAY_TICK_MIN_WAIT: Duration = Duration::from_millis(25);
+
+pub async fn command_run(args: &CommandRunArgs) -> Result<(), CommandRunError> {
+    let mut task_context = TaskContext::new(args.clone()).await?;
+
+    initialize_logger(&task_context)?;
+    let (mut instances, instances_with_digest) = collect_instances(&args.instances)?;
+    task_context.display.set_total_instance(instances.len());
+    if instances_with_digest > 0 {
+        task_context.enable_uploader()?;
+    }
+
+    let task_context = Arc::new(task_context);
+
+    // We will spawn upto `num_parallel_jobs` in parallel. This rate limit is enforced using the
+    // Semaphore `parallel_jobs_sema`. Each task gets sequenced using an own Tokio task, spawned
+    // from `task_main`. We pass the semaphore's permit into this task, in general, the task
+    // may live much longer than the solver. For instance, the task also handles communication
+    // with the stride server and writing into the summary.
+    let num_parallel_jobs = args.parallel_jobs.unwrap() as usize;
+    let parallel_jobs_sema = Arc::new(Semaphore::new(num_parallel_jobs));
+    let mut join_handles = Vec::with_capacity((100 * num_parallel_jobs).min(instances.len()));
+
+    loop {
+        if let Ok(permit) = timeout(
+            DISPLAY_TICK_MIN_WAIT,
+            parallel_jobs_sema.clone().acquire_owned(),
+        )
+            .await
+        {
+            let Some(instance) = instances.next() else {
+                break;
+            };
+
+            if let Ok(permit) = permit {
+                join_handles.push(tokio::spawn(task_main(
+                    task_context.clone(),
+                    instance,
+                    permit,
+                )));
+            } else {
+                error!("Semaphore closed");
+                break;
+            }
+        }
+
+        join_handles.retain(|h| !h.is_finished());
+        task_context
+            .display
+            .tick(num_parallel_jobs - parallel_jobs_sema.available_permits());
+    }
+
+    // at this point, no instance remain to be started, but some solvers can run
+    while parallel_jobs_sema.available_permits() < num_parallel_jobs {
+        task_context
+            .display
+            .tick(num_parallel_jobs - parallel_jobs_sema.available_permits());
+
+        sleep(DISPLAY_TICK_MIN_WAIT).await;
+    }
+
+    task_context.display.switch_to_postprocessing();
+
+    for mut h in join_handles {
+        loop {
+            task_context.display.post_processing_tick();
+            if timeout(DISPLAY_TICK_MIN_WAIT, &mut h).await.is_ok() {
+                break;
+            }
+        }
+    }
+
+    task_context.display.final_message();
+
+    Ok(())
+}
+
+
 
 #[derive(Error, Debug)]
 pub enum CommandRunError {
@@ -36,203 +116,167 @@ pub enum CommandRunError {
     Io(#[from] std::io::Error),
 }
 
-type TaskResult = (JobResult, Option<SolutionInfos>);
-struct RunningTask {
+struct TaskContext {
+    args: CommandRunArgs,
+    display: ProgressDisplay,
+    run_dir: Arc<RunDirectory>,
+    uploader: Option<JobResultUploadAggregation>,
+    summary_writer: SummaryWriter,
+}
+
+impl TaskContext {
+    async fn new(args: CommandRunArgs) -> Result<Self, CommandRunError> {
+        let run_dir = RunDirectory::new()?;
+
+        let display = ProgressDisplay::new(0);
+
+        let summary_writer = SummaryWriter::new(&run_dir.path().join("summary.json")).await?;
+
+        Ok(Self {
+            args,
+            display,
+            run_dir: Arc::new(run_dir),
+            uploader: None,
+            summary_writer,
+        })
+    }
+
+    fn enable_uploader(&mut self) -> Result<(), CommandRunError> {
+        assert!(self.uploader.is_none());
+
+        let uploader = Arc::new(UploadToStride::new_with_server(
+            self.args.solution_server.clone(),
+        )?);
+
+        self.uploader = Some(JobResultUploadAggregation::new(uploader));
+
+        Ok(())
+    }
+}
+
+async fn task_main(
+    context: Arc<TaskContext>,
     instance: Instance,
-    processor: Arc<JobProcessor>,
-    job_progress_bar: JobProgressBar,
-    task: tokio::task::JoinHandle<TaskResult>,
+    permit: OwnedSemaphorePermit,
+) -> Result<(), CommandRunError> {
+    let processor = Arc::new(
+        JobProcessorBuilder::default()
+            .run_directory(context.run_dir.clone())
+            .solver(context.args.solver.clone())
+            .solver_args(context.args.solver_args.clone())
+            .soft_timeout(context.args.soft_timeout)
+            .grace_period(context.args.grace_period)
+            .instance_path(instance.path().to_path_buf())
+            .profiler(!context.args.no_profile)
+            .set_stride_envs(!context.args.no_envs)
+            .build()
+            .unwrap(),
+    );
+
+    let task = {
+        let processor = processor.clone();
+        tokio::spawn(async move { processor.run().await })
+    };
+
+    let mut job_progress_bar = JobProgressBar::new(
+        String::from(
+            processor
+                .instance_path()
+                .file_stem()
+                .and_then(|s| s.to_str())
+                .unwrap_or("unnamed"),
+        ),
+        processor.soft_timeout(),
+        processor.grace_period(),
+    );
+
+    while !task.is_finished() {
+        let progress = processor.progress();
+        job_progress_bar.update_progress_bar(&context.display, progress);
+
+        sleep(DISPLAY_TICK_MIN_WAIT).await;
+    }
+
+    // we only reach this point, if the task finished; so awaiting it should be fast
+    let (job_result, mut opt_info) = task.await.unwrap();
+    job_progress_bar.finish(&context.display, job_result);
+
+    // all remaining steps require very little compute -- so we drop the rate limit permit
+    // to free the resources needed for a new solver run
+    drop(permit);
+
+    // upload and fetch best known
+    let upload_desc = if !context.args.offline
+        && let Some(idigest) = instance.idigest()
+    {
+        let runtime = processor.runtime().expect("failed to get runtime"); // runtime will always be set if the child terminated, independently of successes
+        prepare_upload_descriptor(idigest, runtime, job_result, &mut opt_info)
+    } else {
+        None
+    };
+
+    let best_known = if let Some(uploader) = context.uploader.as_ref()
+        && let Some(desc) = upload_desc
+    {
+        uploader.upload_and_fetch_best_known(desc).await
+    } else {
+        None
+    };
+
+    if let Err(e) = context
+        .summary_writer
+        .add_entry(&instance, job_result, opt_info, best_known)
+        .await
+    {
+        error!("SummaryWriter error: {e:?}");
+    }
+
+    Ok(())
 }
 
-impl RunningTask {
-    fn new(instance: Instance, processor: Arc<JobProcessor>) -> Self {
-        let moved_processor = processor.clone();
-        let task = tokio::spawn(async move { moved_processor.run().await });
-        let job_progress_bar = JobProgressBar::new(
-            String::from(
-                processor
-                    .instance_path()
-                    .file_stem()
-                    .and_then(|s| s.to_str())
-                    .unwrap_or("unnamed"),
-            ),
-            processor.soft_timeout(),
-            processor.grace_period(),
-        );
-
-        Self {
-            instance,
-            processor,
-            job_progress_bar,
-            task,
+fn prepare_upload_descriptor(idigest: InstanceDigest, runtime: Duration, job_result: JobResult, opt_info: &mut Option<SolutionInfos>) -> Option<JobDescription> {
+    match job_result {
+        JobResult::Valid { .. } => {
+            if let Some(opt_info) = opt_info {
+                let mut trees = std::mem::take(&mut opt_info.0);
+                Some(JobDescription::valid_from_strings(
+                    idigest,
+                    &mut trees,
+                    Some(runtime),
+                ))
+            } else {
+                None
+            }
         }
-    }
-
-    fn is_finished(&mut self, display: &ProgressDisplay, now: tokio::time::Instant) -> bool {
-        let progress = self.processor.progress();
-        self.job_progress_bar
-            .update_progress_bar(display, progress, now);
-
-        self.task.is_finished()
-    }
-
-    fn finish(self, display: &mut ProgressDisplay) -> (Instance, TaskResult) {
-        debug!("{:?} block_on task", self.processor.instance_path());
-        let result = block_in_place(|| futures::executor::block_on(self.task)).unwrap();
-        self.job_progress_bar.finish(display, result.0);
-        (self.instance, result)
+        JobResult::Infeasible => Some(JobDescription::infeasible(idigest, Some(runtime))),
+        JobResult::Timeout => Some(JobDescription::timeout(idigest, runtime)),
+        _ => None,
     }
 }
 
-pub async fn command_run(args: &CommandRunArgs) -> Result<(), CommandRunError> {
-    let arc_run_dir = Arc::new(RunDirectory::new()?);
+fn collect_instances(
+    args_instances: &[PathBuf],
+) -> Result<(IntoIter<Instance>, usize), CommandRunError> {
+    let mut instances = Instances::default();
+    for p in args_instances {
+        instances.parse_and_insert_path(p)?;
+    }
+    let instances_with_digest = instances.iter().filter_map(|i| i.idigest()).count();
+    info!(
+        "Found {} instances. Of those {} have an idigest",
+        instances.len(),
+        instances_with_digest
+    );
 
-    let log_file = File::create(arc_run_dir.path().join("messages.log"))?;
+    Ok((instances.into_iter(), instances_with_digest))
+}
+
+fn initialize_logger(task_context: &TaskContext) -> Result<(), CommandRunError> {
+    let log_file = File::create(task_context.run_dir.path().join("messages.log"))?;
     tracing_subscriber::fmt()
         .with_ansi(false)
         .with_writer(log_file)
         .with_max_level(tracing::Level::TRACE)
         .init();
-
-    let (mut instances, instances_with_digest) = {
-        let mut instances = Instances::default();
-        for p in &args.instances {
-            instances.parse_and_insert_path(p)?;
-        }
-        let instances_with_digest = instances.iter().filter_map(|i| i.idigest()).count();
-        info!(
-            "Found {} instances. Of those {} have an idigest",
-            instances.len(),
-            instances_with_digest
-        );
-
-        (instances.into_iter(), instances_with_digest)
-    };
-
-    let upload_aggr = if instances_with_digest > 0 && !args.offline {
-        let uploader = Arc::new(UploadToStride::new_with_server(
-            args.solution_server.clone(),
-        )?);
-        Some(Arc::new(JobResultUploadAggregation::new(uploader)))
-    } else {
-        None
-    };
-
-    let mut display = ProgressDisplay::new(instances.len());
-
-    let summary_writer =
-        Arc::new(SummaryWriter::new(&arc_run_dir.path().join("summary.json")).await?);
-
-    let parallel_jobs = args.parallel_jobs.unwrap() as usize; // the argument parser ensures this value is always set
-    let mut running_tasks = Vec::with_capacity(parallel_jobs);
-    let mut join_handles = Vec::with_capacity(parallel_jobs);
-    loop {
-        if running_tasks.len() < parallel_jobs {
-            if let Some(instance) = instances.next() {
-                // attempt to spawn new task
-                let processor = Arc::new(
-                    JobProcessorBuilder::default()
-                        .run_directory(arc_run_dir.clone())
-                        .solver(args.solver.clone())
-                        .solver_args(args.solver_args.clone())
-                        .soft_timeout(args.soft_timeout)
-                        .grace_period(args.grace_period)
-                        .instance_path(instance.path().to_path_buf())
-                        .profiler(!args.no_profile)
-                        .set_stride_envs(!args.no_envs)
-                        .build()
-                        .unwrap(),
-                );
-
-                running_tasks.push(RunningTask::new(instance, processor));
-            } else if running_tasks.is_empty() {
-                // no running tasks available and no new tasks to spin up
-                break;
-            }
-        }
-
-        let now = tokio::time::Instant::now();
-
-        {
-            let mut idx = 0;
-            while idx < running_tasks.len() {
-                if running_tasks[idx].is_finished(&display, now) {
-                    // task finished
-                    let (instance, (job_result, mut opt_info)) =
-                        running_tasks.swap_remove(idx).finish(&mut display);
-
-                    let upload_desc = if !args.offline
-                        && let Some(idigest) = instance.idigest()
-                    {
-                        let runtime = Duration::from_millis(123);
-                        match job_result {
-                            JobResult::Valid { .. } => {
-                                if let Some(opt_info) = &mut opt_info {
-                                    let mut trees = std::mem::take(&mut opt_info.0);
-                                    Some(JobDescription::valid_from_strings(
-                                        idigest,
-                                        &mut trees,
-                                        Some(runtime),
-                                    ))
-                                } else {
-                                    None
-                                }
-                            }
-                            JobResult::Infeasible => {
-                                Some(JobDescription::infeasible(idigest, Some(runtime)))
-                            }
-                            JobResult::Timeout => Some(JobDescription::timeout(idigest, runtime)),
-                            _ => None,
-                        }
-                    } else {
-                        None
-                    };
-
-                    if let Some(upload_aggr) = upload_aggr.as_ref()
-                        && let Some(desc) = upload_desc
-                    {
-                        let upload_aggr = upload_aggr.clone();
-                        let summary_writer = summary_writer.clone();
-                        join_handles.push(tokio::spawn(async move {
-                            let result = upload_aggr.upload_and_fetch_best_known(desc).await;
-                            if let Err(e) = summary_writer
-                                .add_entry(&instance, job_result, opt_info, result)
-                                .await
-                            {
-                                error!("SummaryWriter error: {e:?}");
-                            }
-                        }));
-                    } else if let Err(e) = summary_writer
-                        .add_entry(&instance, job_result, opt_info, None)
-                        .await
-                    {
-                        error!("SummaryWriter error: {e:?}");
-                    }
-                } else {
-                    idx += 1;
-                }
-            }
-        }
-
-        join_handles.retain(|h| !h.is_finished());
-
-        display.tick(running_tasks.len());
-
-        sleep(Duration::from_millis(5)).await
-    }
-
-    display.switch_to_postprocessing();
-
-    for mut h in join_handles {
-        loop {
-            display.post_processing_tick();
-            if timeout(Duration::from_millis(20), &mut h).await.is_ok() {
-                break;
-            }
-        }
-    }
-
-    display.final_message();
-
     Ok(())
 }
