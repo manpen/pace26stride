2dcdad4e1cf4fb824a187c782aada880dc67842c
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2026-02-21 11:57:38 +0100

    Fix underflow issue in compute_pace_heuristic_score

--- src/commands/run/pace.rs
+++ src/commands/run/pace.rs
@@ -3,8 +3,9 @@ pub fn compute_pace_heuristic_score(
     best_known: usize,
     num_leaves: usize,
 ) -> f64 {
-    let best_known = best_known.min(solver_score);
-    let upper = num_leaves.min(2 * best_known);
+    let solver_score = solver_score as i64;
+    let best_known = (best_known as i64).min(solver_score);
+    let upper = (num_leaves as i64).min(2 * best_known);
 
     ((upper - solver_score) as f64 / (upper - best_known) as f64)
         .max(0.0)
@@ -21,6 +22,7 @@ mod test {
         assert_eq!(compute_pace_heuristic_score(2, 3, 5), 1.0);
         assert_eq!(compute_pace_heuristic_score(5, 3, 5), 0.0);
         assert_eq!(compute_pace_heuristic_score(4, 2, 5), 0.0);
+        assert_eq!(compute_pace_heuristic_score(15, 7, 48), 0.0);
         assert!((0.1..0.9).contains(&compute_pace_heuristic_score(3, 2, 5)));
     }
 }
